5bfc672ed62e361382b5284258916ca1d65a7830
LyThienVuong <vuongly@LyThienVuongs-Mac-Pro.local>
2021-10-13 11:27:23 +0700

    [vuong-photoview] Change image

--- VPhotoview/VPhotoview/ViewController.m
+++ VPhotoview/VPhotoview/ViewController.m
@@ -14,70 +14,85 @@
 @implementation ViewController
 @synthesize photoScrollView;
 int numImages = 0;
-CGFloat kScrollObjHeight = 480.0;
-CGFloat kScrollObjWidth  = 320.0;
+CGFloat scrollHeight = 0;
+CGFloat scrollWidth  = 0;
+UIImageView *imageView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [photoScrollView setBackgroundColor:[UIColor blackColor]];
-    [photoScrollView setCanCancelContentTouches:YES];
-    photoScrollView.indicatorStyle = UIScrollViewIndicatorStyleWhite;
-    photoScrollView.clipsToBounds = YES;
     photoScrollView.scrollEnabled = YES;
     photoScrollView.pagingEnabled = YES;
-    [self loadImageToScrollView];
-    [self setPositionImageToScrollView];
+    
+    [self loadImageViewToScrollView];
+    [self setPositionImageViewOnScrollView];
 }
 
-- (void)loadImageToScrollView {
-    kScrollObjHeight = self.view.frame.size.height;
-    kScrollObjWidth = self.view.frame.size.width;
+- (void)loadImageViewToScrollView {
+    scrollHeight = self.view.frame.size.height;
+    scrollWidth = self.view.frame.size.width;
     
     for(int i = 1; i <= 5; i++){
         NSString *imageName = [NSString stringWithFormat:@"image%i.jpg", i];
-        NSLog(@"++++++++++Image name: %@", imageName);
-        
         UIImage *image = [UIImage imageNamed:imageName];
         if(image==nil)
             break;
-        UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+        
+        // Create imageView and add image
+        imageView = [[UIImageView alloc] initWithImage:image];
         [imageView setContentMode:UIViewContentModeScaleAspectFit];
-        [photoScrollView addSubview:imageView];
-        
-        CGRect sizemageView = imageView.frame;
-        NSLog(@"++++++++++Image size before: width: %f height: %f", sizemageView.size.width, sizemageView.size.height);
-        sizemageView.size.height = kScrollObjHeight;
-        sizemageView.size.width = kScrollObjWidth;
-        imageView.frame = sizemageView;
-        NSLog(@"++++++++++Image size after: width: %f height: %f", kScrollObjHeight, kScrollObjWidth);
-        
+        CGRect sizeImageView = imageView.frame;
+        sizeImageView.size.height = scrollHeight;
+        sizeImageView.size.width = scrollWidth;
+        imageView.frame = sizeImageView;
         imageView.tag = i;
-        photoScrollView.tag = i;
+        
+        // Create zoom scroll to add imageview
+        UIScrollView *zoomScroll = [[UIScrollView alloc] init];
+        zoomScroll.frame = imageView.frame;
+        [zoomScroll setContentSize:CGSizeMake(scrollWidth, scrollHeight)];
+        zoomScroll.minimumZoomScale = 1.0;
+        zoomScroll.maximumZoomScale = 2.0;
+        zoomScroll.tag = i;
+        zoomScroll.delegate = self;
+        [zoomScroll addSubview:imageView];
+        [zoomScroll setContentOffset:CGPointMake(0, -50)];
+        [photoScrollView addSubview:zoomScroll];
         numImages++;
     }
 }
 
--(void)setPositionImageToScrollView {
-    UIImageView *imageView = nil;
+-(void)setPositionImageViewOnScrollView {
     NSArray *subPhotoView = [photoScrollView subviews];
     CGFloat curLocation = 0;
     
-    for (imageView in subPhotoView) {
-        if([imageView isKindOfClass:[UIImageView class]] && imageView.tag > 0) {
+    for (UIImageView *imageView in subPhotoView) {
+        if([imageView isKindOfClass:[UIScrollView class]]) {
             CGRect newFrame = imageView.frame;
             newFrame.origin = CGPointMake(curLocation, 0);
             imageView.frame = newFrame;
-            curLocation +=kScrollObjWidth;
+            curLocation +=scrollWidth;
         }
     }
-    [photoScrollView setContentSize:CGSizeMake((numImages * kScrollObjWidth), photoScrollView.bounds.size.height)];
+    [photoScrollView setContentSize:CGSizeMake((numImages * scrollWidth), photoScrollView.bounds.size.height)];
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return imageView;
 }
 
 - (IBAction)keyNext:(id)sender {
+    if([photoScrollView contentOffset].x == scrollWidth*(numImages - 1)){
+        return;
+    }
+    photoScrollView.contentOffset = CGPointMake([photoScrollView contentOffset].x + scrollWidth, [photoScrollView contentOffset].y);
 }
 
 - (IBAction)keyPrevious:(id)sender {
+    if([photoScrollView contentOffset].x == 0){
+        return;
+    }
+    photoScrollView.contentOffset = CGPointMake([photoScrollView contentOffset].x - scrollWidth, [photoScrollView contentOffset].y);
 }
 
 
