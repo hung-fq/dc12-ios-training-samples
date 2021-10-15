9ff1f47d671e263226a04f50e3ef142b006e5019
Hung Pham <hungfq@gmail.com>
2021-10-15 09:27:43 +0700

    Format code

--- TCalculator/TCalculator/View Controllers/ViewController.m
+++ TCalculator/TCalculator/View Controllers/ViewController.m
@@ -18,9 +18,9 @@
     [super viewDidLoad];
     [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
     [[NSNotificationCenter defaultCenter]
-       addObserver:self selector:@selector(orientationChanged:)
-       name:UIDeviceOrientationDidChangeNotification
-       object:[UIDevice currentDevice]];
+     addObserver:self selector:@selector(orientationChanged:)
+     name:UIDeviceOrientationDidChangeNotification
+     object:[UIDevice currentDevice]];
     
     [self setNum1:0];
     [self setNum2:0];
@@ -31,46 +31,42 @@
     [self printNumber];
     
 }
-- (void) orientationChanged:(NSNotification *)note
-{
-   UIDevice * device = note.object;
-   switch(device.orientation)
-   {
-       case UIDeviceOrientationPortrait:
-           rootView.backgroundColor = [UIColor whiteColor];
-           NSLog(@"In portrait");
-       break;
 
-       case UIDeviceOrientationPortraitUpsideDown:
-           rootView.backgroundColor = [UIColor greenColor];
-           NSLog(@"In Landscape");
-
-       break;
-
-       default:
-       break;
-   };
+- (void) orientationChanged:(NSNotification *)note {
+    UIDevice * device = note.object;
+    switch(device.orientation) {
+        case UIDeviceOrientationPortrait:
+            rootView.backgroundColor = [UIColor whiteColor];
+            NSLog(@"In portrait");
+            break;
+            
+        case UIDeviceOrientationPortraitUpsideDown:
+            rootView.backgroundColor = [UIColor greenColor];
+            NSLog(@"In Landscape");
+            break;
+            
+        default:
+            break;
+    };
 }
 
--(void)printNumber
-{
+-(void)printNumber {
     [lblText setText:theNumber];
 }
--(void)saveNum1
-{
+
+-(void)saveNum1 {
     num1 = [theNumber integerValue];
     theNumber = @"0";
     //[lblText setText:theNumber];
 }
--(void)checkNum
-{
-    if ([theNumber isEqualToString:@"0"])
-    {
+
+-(void)checkNum {
+    if ([theNumber isEqualToString:@"0"]) {
         theNumber = @"";
     }
 }
--(IBAction)calculate:(id)sender
-{
+
+-(IBAction)calculate:(id)sender {
     num2 = [theNumber integerValue];
     if (operand == PLUS) {
         answer = num1 + num2;
@@ -81,16 +77,16 @@
     } else {
         if (num2 == 0) {
             UIAlertController * alert = [UIAlertController
-                            alertControllerWithTitle:@"Error"
-                                             message:@"Cannot devide by zero!"
-                                      preferredStyle:UIAlertControllerStyleAlert];
+                                         alertControllerWithTitle:@"Error"
+                                         message:@"Cannot devide by zero!"
+                                         preferredStyle:UIAlertControllerStyleAlert];
             UIAlertAction* noButton = [UIAlertAction
-                                    actionWithTitle:@"OK"
-                                              style:UIAlertActionStyleDefault
-                                            handler:^(UIAlertAction * action) {
-                                               //Handle no, thanks button
-                                            }];
-
+                                       actionWithTitle:@"OK"
+                                       style:UIAlertActionStyleDefault
+                                       handler:^(UIAlertAction * action) {
+                //Handle no, thanks button
+            }];
+            
             [alert addAction:noButton];
             [self presentViewController:alert animated:YES completion:nil];
         } else {
@@ -106,92 +102,91 @@
     theNumber = @"0";
     
 }
--(IBAction)clearNum:(id)sender
-{
+
+-(IBAction)clearNum:(id)sender {
     theNumber = @"0";
     [self printNumber];
 }
--(IBAction)setPlus:(id)sender
-{
+
+-(IBAction)setPlus:(id)sender {
     [self saveNum1];
     operand = PLUS;
 }
--(IBAction)setMinus:(id)sender
-{
+
+-(IBAction)setMinus:(id)sender {
     [self saveNum1];
     operand = MINUS;
 }
+
 -(IBAction)setMultiply:(id)sender
 {
     [self saveNum1];
     operand = MULTIPLY;
 }
+
 -(IBAction)setDevide:(id)sender
 {
     [self saveNum1];
     operand = DIVIDE;
 }
 
--(IBAction)press9:(id)sender
-{
+-(IBAction)press9:(id)sender {
     [self checkNum];
     theNumber = [theNumber stringByAppendingString:@"9"];
     [self printNumber];
 }
--(IBAction)press8:(id)sender
-{
+
+-(IBAction)press8:(id)sender {
     [self checkNum];
     theNumber = [theNumber stringByAppendingString:@"8"];
     [self printNumber];
 }
--(IBAction)press7:(id)sender
-{
+
+-(IBAction)press7:(id)sender {
     [self checkNum];
     theNumber = [theNumber stringByAppendingString:@"7"];
     [self printNumber];
 }
--(IBAction)press6:(id)sender
-{
+-(IBAction)press6:(id)sender {
     [self checkNum];
     theNumber = [theNumber stringByAppendingString:@"6"];
     [self printNumber];
 }
--(IBAction)press5:(id)sender
-{
+
+-(IBAction)press5:(id)sender {
     [self checkNum];
     theNumber = [theNumber stringByAppendingString:@"5"];
     [self printNumber];
 }
--(IBAction)press4:(id)sender
-{
+
+-(IBAction)press4:(id)sender {
     [self checkNum];
     theNumber = [theNumber stringByAppendingString:@"4"];
     [self printNumber];
 }
--(IBAction)press3:(id)sender
-{
+
+-(IBAction)press3:(id)sender {
     [self checkNum];
     theNumber = [theNumber stringByAppendingString:@"3"];
     [self printNumber];
 }
--(IBAction)press2:(id)sender
-{
+
+-(IBAction)press2:(id)sender {
     [self checkNum];
     theNumber = [theNumber stringByAppendingString:@"2"];
     [self printNumber];
 }
--(IBAction)press1:(id)sender
-{
+
+-(IBAction)press1:(id)sender {
     [self checkNum];
     theNumber = [theNumber stringByAppendingString:@"1"];
     [self printNumber];
 }
--(IBAction)press0:(id)sender
-{
+
+-(IBAction)press0:(id)sender {
     [self checkNum];
     theNumber = [theNumber stringByAppendingString:@"0"];
     [self printNumber];
 }
-
 
 @end
